42ecde52880c031ae8c1dbe981f5fab06377248c
Samuel <samuelsalami100@gmail.com>
2023-12-09 23:16:12 +0100

    First specs & configuration

---
+++ spec/GameStore.spec.ts
@@ -0,0 +1,31 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { GameStore, useGameStore } from "../src/stores/GameStore";
+
+describe('GameStore', ()=>{
+    let gameStore: GameStore
+
+    beforeEach(()=>{
+        gameStore = useGameStore.getState()
+        gameStore.reset();
+        console.log(gameStore);
+        
+    })
+
+    describe('getCellContent', ()=>{
+        it('return the cell content at one-based index specified coord', ()=>{
+            const testBoard = structuredClone(gameStore.board)
+            testBoard[2][2] = 'WALL'
+            useGameStore.setState({board: testBoard})
+
+            expect(gameStore.getCellContent({x:3, y:3})).equal('WALL')
+        })
+    })
+
+    describe('placeRobot(...)', ()=>{
+        it('places the robot at specified coords', ()=>{
+            gameStore.placeRobot({x: 3, y: 3})
+            expect(gameStore.getCellContent({x:3, y:3})).equal('ROBOT')
+        })
+    })
+
+})
---
+++ src/stores/GameStore.ts
@@ -0,0 +1,52 @@
+import { create } from "zustand"
+
+
+type Position = {x: number, y: number}
+type CellContent = 'ROBOT'|'WALL'
+type Direction = 'NORTH' | 'EAST' | 'SOUTH' | 'WEST'
+
+interface GameState {
+    robotPosition: Position | undefined,
+    robotDirection: Direction
+    board: (CellContent|undefined)[][]
+}
+
+const initialState: GameState = {
+    robotPosition: undefined,
+    robotDirection: 'NORTH',
+    board: Array.from(new Array(5), ()=>new Array(5))
+}
+
+interface GameOperations {
+    report: () => string
+    placeRobot: (pos: Position) => void
+    reset: () => void
+    getCellContent: (pos: Position)=> CellContent|undefined/*
+    
+    placeWall: (pos: Position) => void
+    move: () => void
+    left: () => void
+    right: () => void*/
+}
+
+export type GameStore = GameState & GameOperations
+
+export const useGameStore = create<GameStore>()((set,get) => {
+    return {
+        ...initialState,
+        placeRobot: (pos)=>{ 
+            set((oldState)=>{
+                const newBoard = structuredClone(oldState.board)
+                newBoard[pos.y-1][pos.x-1] = 'ROBOT'
+                return {
+                    board: newBoard
+                }
+            })
+        },
+        report: () => { return "TODO" },
+        reset: () => {},
+        getCellContent: (pos) => {
+            return get().board[pos.y-1][pos.x-1]
+        }
+    }
+})
